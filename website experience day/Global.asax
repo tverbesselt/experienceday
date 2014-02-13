886e2dca2da995ddd405ed0ee7b6df95950416e0
tom <tom.verbesselt@gmail.com>
2014-02-13 15:41:46 +0100

    eerste versie mvc + google authentication

---
+++ website experience day/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="website_experience_day.MvcApplication" Language="C#" %>
---
+++ website experience day/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(website_experience_day.Startup))]
+namespace website_experience_day
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
